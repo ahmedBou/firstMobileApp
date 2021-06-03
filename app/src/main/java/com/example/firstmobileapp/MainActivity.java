f752c14b25a64cee377855819cf22ec4deea92b4
ahmed boutayeb <instinctauto@gmail.com>
2021-06-03 17:53:03 +0100

    apply the validation form

--- app/src/main/java/com/example/firstmobileapp/MainActivity.java
+++ app/src/main/java/com/example/firstmobileapp/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.service.autofill.RegexValidator;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
@@ -40,8 +43,35 @@ public class MainActivity extends AppCompatActivity {
         //add validation mobile
         awesomeValidation.addValidation(this,R.id.et_mobile,
                 "[5-9]{1}[0-9]{9}$",R.string.invalid_mobile);
-        
 
+        //add validation email
+        awesomeValidation.addValidation(this,R.id.et_email,
+                Patterns.EMAIL_ADDRESS, R.string.invalid_email);
+        // for website
+        awesomeValidation.addValidation(this,R.id.et_email,
+                Patterns.WEB_URL, R.string.invalid_website);
+        // for password
+        awesomeValidation.addValidation(this,R.id.et_mobile,
+                ".{6,}",R.string.invalid_password);
+        // for confirm password
+        awesomeValidation.addValidation(this, R.id.et_confirm_password,
+                R.id.et_password, R.string.invalid_confirm_password);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Check validation
+                if(awesomeValidation.validate()){
+                    //On success
+                    Toast.makeText(getApplicationContext(),
+                            "Form validate Successfully", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            "Validation Failed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
     }
 }
